3b9514f4444d5d0fe61a9cfb3adc5d5c0c869d11
sreehari17 <101086245+Sreehari1770@users.noreply.github.com>
2023-07-12 14:52:29 +0530

    day 5

---
+++ day 5/footer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './footer.css';
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <h3>BUILT BETTER FURNITURE</h3>
+      <div className="footer__social">
+        <a href="https://www.facebook.com" className="footer_social-icon">
+          <FaFacebook />
+        </a>
+        <a href="https://www.twitter.com" className="footer_social-icon">
+          <FaTwitter />
+        </a>
+        <a href="https://www.instagram.com" className="footer_social-icon">
+          <FaInstagram />
+        </a>
+      </div>
+      <p>
+        &copy; 2023 Your Built better Furniture Store. All rights reserved. | 
+        <a href="/privacy-policy" className="footer_link">Privacy Policy</a> | 
+        <a href="/terms-of-service" className="footer_link">Terms of Service</a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
